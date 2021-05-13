6b8cb2ca4b96af027c6af2704fa4f5adfc77605d
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2021-05-13 12:22:46 -0300

    first commit

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+const {runtime, AutomaticPrefetchPlugin} = require("webpack")
+
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-typescript',
+    [
+      '@babel/preset-react', {
+        runtime: 'automatic'
+      }
+    ]
+  ]
+}
---
+++ dist/bundle.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _App = _interopRequireDefault(require("./App"));
+
+var _jsxRuntime = require("react/jsx-runtime");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var Index = function Index() {
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)(_App["default"], {});
+};
+
+var _default = Index;
+exports["default"] = _default;
